9ddee442a10a0c544f4d249737256f13d73370a0
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:43:47 +0300

    Add files via upload

---
+++ Exam-Examples/15-16-June-2019/02.MovieDay/02.MovieDay/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _02.MovieDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = int.Parse(Console.ReadLine());
+            int scenes = int.Parse(Console.ReadLine());
+            int timePerScene = int.Parse(Console.ReadLine());
+
+            double preparation = time * 15 / 100;
+            double neededTime = scenes * timePerScene + preparation;
+
+            if (time>=neededTime)
+            {
+                Console.WriteLine($"You managed to finish the movie" +
+                    $" on time! You have {Math.Round(time-neededTime)} minutes left!");
+            }
+            else
+            {
+                Console.WriteLine($"Time is up! To complete the movie " +
+                    $"you need {Math.Round(neededTime-time)} minutes.");
+            }
+        }
+    }
+}
