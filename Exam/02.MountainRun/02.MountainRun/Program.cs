dfaf3dbe91a1ce3e4475780c04a72bdca02c32a6
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 16:31:31 +0300

    Add files via upload

---
+++ Exam/02.MountainRun/02.MountainRun/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02.MountainRun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordInSeconds = double.Parse(Console.ReadLine());
+            double distanceInMeters = double.Parse(Console.ReadLine());
+            double timeInSecondsPerMeter = double.Parse(Console.ReadLine());
+
+            double time = distanceInMeters * timeInSecondsPerMeter + Math.Floor(distanceInMeters / 50)*30;
+
+            if (time >= recordInSeconds)
+            {
+                Console.WriteLine($"No! He was {time - recordInSeconds:F2} seconds slower.");
+            }
+            else
+            {
+                Console.WriteLine($"Yes! The new record is {time:F2} seconds.");
+            }
+        }
+    }
+}
