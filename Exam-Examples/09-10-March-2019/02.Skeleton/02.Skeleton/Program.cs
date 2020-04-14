b0235fa155f8d9ebab8f171bf31976aa10f83217
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:32:17 +0300

    Add files via upload

---
+++ Exam-Examples/09-10-March-2019/02.Skeleton/02.Skeleton/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _02.Skeleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int minutes = int.Parse(Console.ReadLine());
+            int seconds = int.Parse(Console.ReadLine());
+            double lengthInMeters = double.Parse(Console.ReadLine());
+            int secondsPer100Meters = int.Parse(Console.ReadLine());
+
+            int time = minutes * 60 + seconds;
+            double hisTime = lengthInMeters * secondsPer100Meters / 100 - lengthInMeters / 120 * 2.5;
+            if (time>=hisTime)
+            {
+                Console.WriteLine($"Marin Bangiev won an Olympic quota!");
+                Console.WriteLine($"His time is {hisTime:F3}.");
+            }
+            else
+            {
+                Console.WriteLine($"No, Marin failed! He was {(hisTime-time):F3} second slower.");
+            }
+        }
+    }
+}
