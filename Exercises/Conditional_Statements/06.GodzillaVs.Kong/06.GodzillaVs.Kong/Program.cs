5e9380706856f807079d346e4ad3f6a01f22b529
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 18:18:01 +0300

    Add files via upload

---
+++ Exercises/Conditional_Statements/06.GodzillaVs.Kong/06.GodzillaVs.Kong/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _06.GodzillaVs.Kong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int statists = int.Parse(Console.ReadLine());
+            double statistPrice = double.Parse(Console.ReadLine());
+
+            double decor = budget * 10 / 100;
+            if (statists>150)
+            {
+                double statistDiscount = statistPrice - statistPrice * 10 / 100;
+                double sum = statists * statistDiscount + decor;
+                if (sum>budget)
+                {
+                    Console.WriteLine("Not enough money!");
+                    Console.WriteLine($"Wingard needs {(sum-budget):F2} leva more.");
+                }
+                else
+                {
+                    Console.WriteLine("Action!");
+                    Console.WriteLine($"Wingard starts filming with {(budget - sum):F2} leva left.");
+
+                }
+            }
+            else
+            {
+                double sum = statistPrice * statists + decor;
+                if (sum > budget)
+                {
+                    Console.WriteLine("Not enough money!");
+                    Console.WriteLine($"Wingard needs {(sum - budget):F2} leva more.");
+                }
+                else
+                {
+                    Console.WriteLine("Action!");
+                    Console.WriteLine($"Wingard starts filming with {(budget-sum):F2} leva left.");
+                }
+            }
+        }
+    }
+}
