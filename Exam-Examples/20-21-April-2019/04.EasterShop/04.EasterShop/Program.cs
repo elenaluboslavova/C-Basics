2cf126afeead5ca369912a0c6c9e840e6b1bf696
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 18:02:01 +0300

    Add files via upload

---
+++ Exam-Examples/20-21-April-2019/04.EasterShop/04.EasterShop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _04.EasterShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int eggs = int.Parse(Console.ReadLine());
+
+            string command = "";
+            int eggsShop = 0;
+            int sold = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                command = Console.ReadLine();
+                if (command == "Close")
+                {
+                    Console.WriteLine("Store is closed!");
+                    Console.WriteLine($"{sold} eggs sold.");
+                    Environment.Exit(0);
+                }
+                eggsShop = int.Parse(Console.ReadLine());
+
+                if (command == "Buy")
+                {
+                    if (eggsShop > eggs)
+                    {
+                        Console.WriteLine("Not enough eggs in store!");
+                        Console.WriteLine($"You can buy only {eggs}.");
+                        Environment.Exit(0);
+                    }
+                    sold += eggsShop;
+                    eggs -= eggsShop;
+                }
+                if (command == "Fill")
+                {
+                    eggs += eggsShop;
+                }
+            }
+        }
+    }
+}
