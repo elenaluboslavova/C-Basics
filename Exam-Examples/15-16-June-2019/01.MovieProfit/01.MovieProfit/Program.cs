d4fc1886704e1a0f58232ef7435acaf7a83c4480
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:41:06 +0300

    Add files via upload

---
+++ Exam-Examples/15-16-June-2019/01.MovieProfit/01.MovieProfit/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01.MovieProfit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int days = int.Parse(Console.ReadLine());
+            int ticketsCount = int.Parse(Console.ReadLine());
+            double ticketPrice = double.Parse(Console.ReadLine());
+            int percent = int.Parse(Console.ReadLine());
+
+            double sum = days * ticketsCount * ticketPrice * percent / 100;
+            double finalSum = days * ticketPrice * ticketsCount - sum;
+            Console.WriteLine($"The profit from the movie {name} is {finalSum:F2} lv.");
+        }
+    }
+}
