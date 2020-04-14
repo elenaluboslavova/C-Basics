91e1412a7f39d68823dc422931a5cee63bfe5230
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 16:37:05 +0300

    Add files via upload

---
+++ Exam/05.CareOfPuppy/05.CareOfPuppy/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05.CareOfPuppy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int foodQuantityInKg = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+            int foodInGr = 0;
+            int eatenFood = 0;
+
+            while (command != "Adopted")
+            {
+                foodInGr = int.Parse(command);
+                eatenFood += foodInGr;
+
+
+                command = Console.ReadLine();
+            }
+            if (eatenFood <= foodQuantityInKg * 1000)
+            {
+                Console.WriteLine($"Food is enough! Leftovers: {foodQuantityInKg * 1000 - eatenFood} grams.");
+            }
+            else
+            {
+                Console.WriteLine($"Food is not enough. You need {eatenFood - foodQuantityInKg * 1000} grams more.");
+            }
+        }
+    }
+}
