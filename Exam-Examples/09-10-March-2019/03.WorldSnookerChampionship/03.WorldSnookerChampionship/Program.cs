ea14da4045ac14e1c413705c29d9ce0f0f6fbac3
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:34:01 +0300

    Add files via upload

---
+++ Exam-Examples/09-10-March-2019/03.WorldSnookerChampionship/03.WorldSnookerChampionship/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace _03.WorldSnookerChampionship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stage = Console.ReadLine();
+            string ticketType = Console.ReadLine();
+            int ticketsCount = int.Parse(Console.ReadLine());
+            char picture = char.Parse(Console.ReadLine());
+
+            double sumPerTicket = 0;
+            double finalSum = 0;
+
+            if (stage == "Quarter final")
+            {
+                switch(ticketType)
+                {
+                    case "Standard":
+                        sumPerTicket = 55.50;
+                        break;
+                    case "Premium":
+                        sumPerTicket = 105.20;
+                        break;
+                    case "VIP":
+                        sumPerTicket = 118.90;
+                        break;
+                }
+            }
+
+            else if (stage == "Semi final")
+            {
+                switch (ticketType)
+                {
+                    case "Standard":
+                        sumPerTicket = 75.88;
+                        break;
+                    case "Premium":
+                        sumPerTicket = 125.22;
+                        break;
+                    case "VIP":
+                        sumPerTicket = 300.40;
+                        break;
+                }
+            }
+
+            else if (stage == "Final")
+            {
+                switch (ticketType)
+                {
+                    case "Standard":
+                        sumPerTicket = 110.10;
+                        break;
+                    case "Premium":
+                        sumPerTicket = 160.66;
+                        break;
+                    case "VIP":
+                        sumPerTicket = 400;
+                        break;
+                }
+            }
+
+            finalSum = sumPerTicket * ticketsCount;
+            
+
+            if (finalSum > 2500 && finalSum <= 4000)
+            {
+                finalSum = finalSum - finalSum * 0.1;
+            }
+            if (picture == 'Y' && finalSum <= 4000)
+            {
+                finalSum += 40 * ticketsCount;
+            }
+            else if (finalSum > 4000)
+            {
+                finalSum = finalSum - finalSum * 0.25;
+            }
+
+            Console.WriteLine($"{finalSum:F2}");
+        }
+    }
+}
