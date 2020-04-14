80b601570ff78770e39563792623cae0bf464d19
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:27:57 +0300

    Add files via upload

---
+++ Exam-Examples/06-07-July-2019/06.TheMostPowerfulWord/06.TheMostPowerfulWord/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _06.TheMostPowerfulWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputWord = Console.ReadLine();
+
+            int currentLetter = 0;
+            int currentLetterPower = 0;
+
+            string mostPowerfulWord = "";
+            int mostPowerfulWordPower = 0;
+
+            while (inputWord != "End of words")
+            {
+                currentLetterPower = 0;
+                currentLetter = 0;
+                for (int i = 0; i < inputWord.Length; i++)
+                {
+                    currentLetter = (int)inputWord[i];
+                    currentLetterPower += currentLetter;
+                }
+
+                bool vowel = inputWord[0] == 'a' || inputWord[0] == 'e' ||
+                       inputWord[0] == 'i' || inputWord[0] == 'o' || inputWord[0] == 'u' ||
+                       inputWord[0] == 'y' || inputWord[0] == 'A' || inputWord[0] == 'E' ||
+                       inputWord[0] == 'I' || inputWord[0] == 'O' || inputWord[0] == 'U' ||
+                       inputWord[0] == 'Y'; 
+
+                if (vowel)
+                {
+                    currentLetterPower *= inputWord.Length;
+                }
+                else
+                {
+                    currentLetterPower /= inputWord.Length;
+                }
+                if (currentLetterPower > mostPowerfulWordPower)
+                {
+                    mostPowerfulWordPower = currentLetterPower;
+                    mostPowerfulWord = inputWord;
+                }
+
+
+
+                inputWord = Console.ReadLine();
+            }
+            Console.WriteLine($"The most powerful word is {mostPowerfulWord} - {mostPowerfulWordPower:F0}");
+        }
+    }
+}
