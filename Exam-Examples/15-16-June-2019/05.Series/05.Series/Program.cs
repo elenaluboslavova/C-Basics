7d959fd6f35f41c60c0ce6f1a233977f2b50a627
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:48:50 +0300

    Add files via upload

---
+++ Exam-Examples/15-16-June-2019/05.Series/05.Series/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _05.Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+
+            double finalPrice = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine());
+
+                switch(name)
+                {
+                    case "Thrones":
+                        price = price * 0.5;
+                        break;
+                    case "Lucifer":
+                        price = price * 0.6;
+                        break;
+                    case "Protector":
+                        price = price * 0.7;
+                        break;
+                    case "TotalDrama":
+                        price = price * 0.8;
+                        break;
+                    case "Area":
+                        price = price * 0.9;
+                        break;
+                }
+                finalPrice += price;
+            }
+            if (budget >= finalPrice)
+            {
+                Console.WriteLine($"You bought all the series and left with {budget-finalPrice:F2} lv.");
+            }
+            else
+            {
+                Console.WriteLine($"You need {finalPrice-budget:F2} lv. more to buy the series!");
+            }
+        }
+    }
+}
