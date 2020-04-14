578ccf0225805c7fd7c14d318f2d04f3a71a3767
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 16:54:56 +0300

    Add files via upload

---
+++ Exam-Examples/02-03-May-2019/01.FoodDelivery/01.FoodDelivery/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01.FoodDelivery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int chickenMenuCount = int.Parse(Console.ReadLine());
+            int fishMenuCount = int.Parse(Console.ReadLine());
+            int vegeMenuCount = int.Parse(Console.ReadLine());
+
+            double sum = chickenMenuCount * 10.35 + fishMenuCount * 12.40 +
+                vegeMenuCount * 8.15;
+            double dessert = sum * 20 / 100;
+            double finalSum = sum + dessert + 2.50;
+            Console.WriteLine($"Total: {finalSum:F2}");
+        }
+    }
+}
