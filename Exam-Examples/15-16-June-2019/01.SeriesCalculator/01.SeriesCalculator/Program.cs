368903ce0d86a8b561d24f7cbe9e4a10c3e3d000
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:42:03 +0300

    Add files via upload

---
+++ Exam-Examples/15-16-June-2019/01.SeriesCalculator/01.SeriesCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.SeriesCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int seasons = int.Parse(Console.ReadLine());
+            int episodes = int.Parse(Console.ReadLine());
+            int duration = int.Parse(Console.ReadLine());
+
+            double ad = duration * 20 / 100;
+            double time = seasons * episodes * (duration + ad) + seasons * 10;
+            Console.WriteLine($"Total time needed to watch the {name} series is {Math.Floor(time)} minutes.");
+        }
+    }
+}
