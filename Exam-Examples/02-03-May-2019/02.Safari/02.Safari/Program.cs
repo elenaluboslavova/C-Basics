00e8db7f6655d28f7c270c63464b1f6fb19396b7
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:00:08 +0300

    Add files via upload

---
+++ Exam-Examples/02-03-May-2019/02.Safari/02.Safari/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _02.Safari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            double fuel = double.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            if (day=="Saturday")
+            {
+                double sum = fuel * 2.10 + 100 - (fuel * 2.10 + 100) * 10 / 100;
+                if (sum>budget)
+                {
+                    Console.WriteLine($"Not enough money! Money" +
+                        $" needed: {(sum-budget):F2} lv.");
+                }
+                else
+                {
+                    Console.WriteLine($"Safari time! Money left:" +
+                        $" {(budget-sum):F2} lv.");
+                }
+            }
+            else if (day=="Sunday")
+            {
+                double sum = fuel * 2.10 + 100 - (fuel * 2.10 + 100) * 20 / 100;
+                if (sum > budget)
+                {
+                    Console.WriteLine($"Not enough money! Money" +
+                        $" needed: {(sum - budget):F2} lv.");
+                }
+                else
+                {
+                    Console.WriteLine($"Safari time! Money left:" +
+                        $" {(budget - sum):F2} lv.");
+                }
+            }
+        }
+    }
+}
