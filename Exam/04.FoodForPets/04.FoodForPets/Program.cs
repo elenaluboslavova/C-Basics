acc69eda1a7320baa7610d4c49293a8c91ceead4
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 16:35:46 +0300

    Add files via upload

---
+++ Exam/04.FoodForPets/04.FoodForPets/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04.FoodForPets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            double foodQuantity = double.Parse(Console.ReadLine());
+
+            int dogFood = 0;
+            int catFood = 0;
+            double biscuits = 0;
+            int allDogFood = 0;
+            int allCatFood = 0;
+            double allBiscuits = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                dogFood = int.Parse(Console.ReadLine());
+                catFood = int.Parse(Console.ReadLine());
+
+                allDogFood += dogFood;
+                allCatFood += catFood;
+                if (i % 3 == 0)
+                {
+                    biscuits = (dogFood + catFood) * 0.1;
+                    allBiscuits += biscuits;
+                }
+            }
+            Console.WriteLine($"Total eaten biscuits: {allBiscuits:F0}gr.");
+            Console.WriteLine($"{(allDogFood + allCatFood) / foodQuantity * 100:F2}% of the food has been eaten.");
+            Console.WriteLine($"{allDogFood * 1.0 / (allDogFood + allCatFood) * 100:F2}% eaten from the dog.");
+            Console.WriteLine($"{allCatFood * 1.0 / (allDogFood + allCatFood) * 100:F2}% eaten from the cat.");
+        }
+    }
+}
