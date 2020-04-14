68f5dca11867690353d92e1cce07ad14ed86a023
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:36:39 +0300

    Add files via upload

---
+++ Exam-Examples/09-10-March-2019/04.GameNumberWars/04.GameNumberWars/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace _04.GameNumberWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameFirst = Console.ReadLine();
+            string nameSecond = Console.ReadLine();
+
+            int cardFirst = 0;
+            int cardSecond = 0;
+
+            int pointsFirst = 0;
+            int pointsSecond = 0;
+
+            int finalFirst = 0;
+            int finalSecond = 0;
+
+            string command = Console.ReadLine();
+            while (command != "End of game")
+            {
+                cardFirst = int.Parse(command);
+                cardSecond = int.Parse(Console.ReadLine());
+
+                if (cardFirst == cardSecond)
+                {
+                    cardFirst = int.Parse(Console.ReadLine());
+                    cardSecond = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Number wars!");
+                    if (cardFirst > cardSecond)
+                    {
+                        Console.WriteLine($"{nameFirst} is winner with {finalFirst} points");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{nameSecond} is winner with {finalSecond} points");
+                        return;
+                    }
+
+                    
+                }
+                if (cardFirst > cardSecond)
+                {
+                    pointsFirst = cardFirst - cardSecond;
+                    finalFirst += pointsFirst;
+                }
+                else if (cardSecond > cardFirst)
+                {
+                    pointsSecond = cardSecond - cardFirst;
+                    finalSecond += pointsSecond;
+                }
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"{nameFirst} has {finalFirst} points");
+            Console.WriteLine($"{nameSecond} has {finalSecond} points");
+        }
+    }
+}
