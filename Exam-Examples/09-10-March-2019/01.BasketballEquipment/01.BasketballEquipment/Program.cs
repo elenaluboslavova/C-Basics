0cbf8ca92833beeee3e31b384b2cf8912cd9dd90
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:29:07 +0300

    Add files via upload

---
+++ Exam-Examples/09-10-March-2019/01.BasketballEquipment/01.BasketballEquipment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.BasketballEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int feePerYear = int.Parse(Console.ReadLine());
+
+            double shoes = feePerYear - feePerYear * 40 / 100;
+            double outfit = shoes - shoes * 20 / 100;
+            double ball = outfit * 1 / 4;
+            double accessories = ball * 1 / 5;
+            double sum = shoes + outfit + ball + accessories + feePerYear;
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
