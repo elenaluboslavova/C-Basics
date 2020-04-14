5c02d571ca29bd9854e96d42619308d86609396a
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 18:16:18 +0300

    Add files via upload

---
+++ Exercises/Conditional_Statements/04.MetricConverter/04.MetricConverter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _04.MetricConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            string m1 = Console.ReadLine();
+            string m2 = Console.ReadLine();
+
+            if (m1=="mm" && m2=="m")
+            {
+                Console.WriteLine($"{(number / 1000):F3}");
+            }
+            else if(m1=="mm" && m2=="cm")
+            {
+                Console.WriteLine($"{(number / 10):F3}");
+            }
+            else if (m1 == "cm" && m2 == "m")
+            {
+                Console.WriteLine($"{(number / 100):F3}");
+            }
+            else if (m1 == "cm" && m2 == "mm")
+            {
+                Console.WriteLine($"{(number * 10):F3}");
+            }
+            else if (m1 == "m" && m2 == "cm")
+            {
+                Console.WriteLine($"{(number * 100):F3}");
+            }
+            else if (m1 == "m" && m2 == "mm")
+            {
+                Console.WriteLine($"{(number * 1000):F3}");
+            }
+        }
+    }
+}
