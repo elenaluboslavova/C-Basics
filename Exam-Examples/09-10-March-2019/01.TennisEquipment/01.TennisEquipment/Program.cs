5f0c6660242c71cd45fcfa4fc1596fa5c36a065b
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:30:03 +0300

    Add files via upload

---
+++ Exam-Examples/09-10-March-2019/01.TennisEquipment/01.TennisEquipment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01.TennisEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tennisRacketPrice = double.Parse(Console.ReadLine());
+            int tennisRacketCount = int.Parse(Console.ReadLine());
+            int shoesCount = int.Parse(Console.ReadLine());
+
+            double shoesPrice = tennisRacketPrice * 1 / 6;
+            double sum = tennisRacketPrice * tennisRacketCount +
+                shoesPrice * shoesCount;
+            double otherEquipment = sum * 20 / 100;
+            double finalSum = sum + otherEquipment;
+            double paySum = finalSum * 1 / 8;
+            Console.WriteLine($"Price to be paid by Djokovic {Math.Floor(paySum)}");
+            Console.WriteLine($"Price to be paid by sponsors {Math.Ceiling(finalSum*7/8)}");
+        }
+    }
+}
