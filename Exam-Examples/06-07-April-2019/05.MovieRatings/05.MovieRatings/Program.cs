754ee0edee1bfaf481460f360eb543699748f897
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:13:20 +0300

    Add files via upload

---
+++ Exam-Examples/06-07-April-2019/05.MovieRatings/05.MovieRatings/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _05.MovieRatings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int movies = int.Parse(Console.ReadLine());
+
+            string movieName = "";
+            double movieRating = 0;
+
+            double max = double.MinValue;
+            double min = double.MaxValue;
+            string maxMovie = "";
+            string minMovie = "";
+            double average = 0;
+
+            for (int i = 0; i < movies; i++)
+            {
+                movieName = Console.ReadLine();
+                movieRating = double.Parse(Console.ReadLine());
+                average += movieRating;
+                if (movieRating > max)
+                {
+                    max = movieRating;
+                    maxMovie = movieName;
+                }
+                if (movieRating < min)
+                {
+                    min = movieRating;
+                    minMovie = movieName;
+                }
+            }
+            Console.WriteLine($"{maxMovie} is with highest rating: {max:F1}");
+            Console.WriteLine($"{minMovie} is with lowest rating: {min:F1}");
+            Console.WriteLine($"Average rating: {average / movies:F1}");
+        }
+    }
+}
