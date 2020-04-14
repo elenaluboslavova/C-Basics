469c32dd05d2e3ca3a5da77bc1582a5fd8cdee8e
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 18:08:15 +0300

    Add files via upload

---
+++ Exam-Examples/27-28-July-2019/02.Reservation/02.Reservation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _02.Reservation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int reservationDay = int.Parse(Console.ReadLine());
+            int reservationMonth = int.Parse(Console.ReadLine());
+            int accommodationDay = int.Parse(Console.ReadLine());
+            int accommodationMonth = int.Parse(Console.ReadLine());
+            int leavingDay = int.Parse(Console.ReadLine());
+            int leavingMonth = int.Parse(Console.ReadLine());
+
+            int night = 30;
+            
+            if (accommodationDay == reservationDay + 10)
+            {
+                night = 25;
+            }
+
+            if (accommodationMonth == reservationMonth + 1)
+            {
+                night = (leavingDay - accommodationDay) * 30 - (leavingDay - accommodationDay) * 30 * 20 / 100;
+            }
+            int sum = (leavingDay - accommodationDay) * night;
+
+            Console.WriteLine($"Your stay from {accommodationDay}/" +
+                $"{accommodationMonth} to {leavingDay}/{leavingMonth}" +
+                $" will cost {sum:F2}");
+        }
+    }
+}
