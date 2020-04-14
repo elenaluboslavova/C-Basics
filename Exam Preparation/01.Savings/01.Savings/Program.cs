fcb9674aff9cb544fcd6b9fd0e3835a6bcce9693
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 16:43:02 +0300

    Add files via upload

---
+++ Exam Preparation/01.Savings/01.Savings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.Savings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double monthlyIncome = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double neededSum = double.Parse(Console.ReadLine());
+
+            double sum = (monthlyIncome - neededSum - monthlyIncome * 0.3) * months;
+
+            Console.WriteLine($"She can save {sum / months / monthlyIncome * 100:F2}%");
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
