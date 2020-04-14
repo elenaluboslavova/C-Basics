076a9af1f7a8f8b7ab683be53fcfc92195a1436c
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 18:06:22 +0300

    Add files via upload

---
+++ Exam-Examples/27-28-July-2019/01.LemonadeStand/01.LemonadeStand/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.LemonadeStand
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lemonsKg = double.Parse(Console.ReadLine());
+            double sugarKg = double.Parse(Console.ReadLine());
+            double waterLiters = double.Parse(Console.ReadLine());
+
+            double lemonJuiceMl = lemonsKg * 980;
+            double lemonade = waterLiters * 1000 + lemonJuiceMl + sugarKg * 30 / 100;
+            double cups = Math.Floor(lemonade / 150);
+            double profit = cups * 1.20;
+            Console.WriteLine($"All cups sold: {cups}");
+            Console.WriteLine($"Money earned: {profit:F2}");
+        }
+    }
+}
