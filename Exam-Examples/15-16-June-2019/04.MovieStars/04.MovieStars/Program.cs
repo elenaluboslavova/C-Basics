4dfe6ade3944c67a02d2abe7b08b45bf97d823e6
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:47:13 +0300

    Add files via upload

---
+++ Exam-Examples/15-16-June-2019/04.MovieStars/04.MovieStars/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04.MovieStars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+            double money = 0;
+
+            while (command != "ACTION")
+            {
+                string actor = command;
+                if (actor.Length <= 15)
+                {
+                    money = double.Parse(Console.ReadLine());
+                    budget -= money;
+                }
+                else
+                {
+                    money = budget * 0.2;
+                    budget -= money;
+                }
+
+                if (budget <= 0)
+                {
+                    Console.WriteLine($"We need {Math.Abs(budget):F2} leva for our actors.");
+                    return;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine($"We are left with {budget:F2} leva.");
+        }
+    }
+}
