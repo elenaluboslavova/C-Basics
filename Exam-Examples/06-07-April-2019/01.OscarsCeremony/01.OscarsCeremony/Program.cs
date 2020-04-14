fc1782d0905574032d963caa1906dc70941d910f
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:10:07 +0300

    Add files via upload

---
+++ Exam-Examples/06-07-April-2019/01.OscarsCeremony/01.OscarsCeremony/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.OscarsCeremony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rent = int.Parse(Console.ReadLine());
+
+            double statuettes = rent - rent * 30 / 100;
+            double catering = statuettes - statuettes * 15 / 100;
+            double music = catering * 1 / 2;
+            double sum = rent + statuettes + catering + music;
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
