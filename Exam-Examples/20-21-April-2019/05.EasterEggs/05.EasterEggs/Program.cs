a5b0711354c41ee141d3d1a6cb213fb626841dff
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 18:03:36 +0300

    Add files via upload

---
+++ Exam-Examples/20-21-April-2019/05.EasterEggs/05.EasterEggs/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _05.EasterEggs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int paintedEggs = int.Parse(Console.ReadLine());
+
+            string color = "";
+            int redCounter = 0;
+            int orangeCounter = 0;
+            int blueCounter = 0;
+            int greenCounter = 0;
+
+            int max = int.MinValue;
+
+            string maxColor = "";
+
+            for (int i = 0; i < paintedEggs; i++)
+            {
+                color = Console.ReadLine();
+
+                if (color == "red")
+                {
+                    redCounter++;
+                    if (redCounter > max)
+                    {
+                        max = redCounter;
+                        maxColor = "red";
+                    }
+                }
+                else if (color == "orange")
+                {
+                    orangeCounter++;
+                    if (orangeCounter > max)
+                    {
+                        max = orangeCounter;
+                        maxColor = "orange";
+                    }
+                }
+                else if (color == "blue")
+                {
+                    blueCounter++;
+                    if (blueCounter > max)
+                    {
+                        max = blueCounter;
+                        maxColor = "blue";
+                    }
+                }
+                else if (color == "green")
+                {
+                    greenCounter++;
+                    if (greenCounter > max)
+                    {
+                        max = greenCounter;
+                        maxColor = "green";
+                    }
+                }
+            }
+
+            Console.WriteLine($"Red eggs: {redCounter}");
+            Console.WriteLine($"Orange eggs: {orangeCounter}");
+            Console.WriteLine($"Blue eggs: {blueCounter}");
+            Console.WriteLine($"Green eggs: {greenCounter}");
+            Console.WriteLine($"Max eggs: {max} -> {maxColor}");
+        }
+    }
+}
