307f887bc8aac34abd42fde1cf2c00caf90aa2d3
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:20:27 +0300

    Add files via upload

---
+++ Exam-Examples/06-07-July-2019/02.Shopping/02.Shopping/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02.Shopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int nCount = int.Parse(Console.ReadLine());
+            int mCount = int.Parse(Console.ReadLine());
+            int pCount = int.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            int n = 250;
+            double m = n * nCount * 1.0 * 35 / 100;
+            double p = n * nCount * 1.0 * 1 / 10;
+            sum = n * nCount + m * mCount + p * pCount;
+            if (nCount > mCount)
+            {
+                sum = sum - sum * 15 / 100;
+            }
+
+            if (budget>=sum)
+            {
+                Console.WriteLine($"You have {(budget-sum):F2} leva left!");
+            }
+            else
+            {
+                Console.WriteLine($"Not enough money! You need {(sum-budget):F2}" +
+                    $" leva more!");
+            }
+        }
+    }
+}
