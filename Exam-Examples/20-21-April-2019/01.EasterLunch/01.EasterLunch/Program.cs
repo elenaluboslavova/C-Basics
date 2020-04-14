7c4f7a757502caafa52a3183b09fc86e50cd4173
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:52:57 +0300

    Add files via upload

---
+++ Exam-Examples/20-21-April-2019/01.EasterLunch/01.EasterLunch/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.EasterLunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int easterbread = int.Parse(Console.ReadLine());
+            int eggCartoon = int.Parse(Console.ReadLine());
+            int cookiesKg = int.Parse(Console.ReadLine());
+
+            double sum = easterbread * 3.20 + eggCartoon * 4.35 +
+                cookiesKg * 5.40 + eggCartoon * 12 * 0.15;
+            Console.WriteLine($"{sum*1.0:F2}");
+        }
+    }
+}
