6f86c2f01c0de61fc6d1996684af81d063a287d5
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:39:52 +0300

    Add files via upload

---
+++ Exam-Examples/09-10-March-2019/06.HighJump/06.HighJump/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _06.HighJump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int desiredHigh = int.Parse(Console.ReadLine());
+            int startingHigh = desiredHigh - 30;
+            int shots = 3;
+            bool flagFail = false;
+            int highAtWhichHeFailed = 0;
+            int maxJumpedHigh = 0;
+            int jumps = 0;
+            for (int i = startingHigh; i <= desiredHigh;)
+            {
+                int jumpingHigh = int.Parse(Console.ReadLine());
+                jumps++;
+
+
+                if (!(jumpingHigh > i))
+                {
+                    shots--;
+                    if (shots == 0)
+                    {
+                        highAtWhichHeFailed = i;
+                        flagFail = true;
+                        break;
+                    }
+                }
+                else
+                {
+                    shots = 3;
+                    maxJumpedHigh = i;
+                    i += 5;
+                }
+
+
+            }
+
+            if (flagFail)
+            {
+                Console.WriteLine($"Tihomir failed at {highAtWhichHeFailed}cm after {jumps} jumps.");
+            }
+            else
+            {
+                Console.WriteLine($"Tihomir succeeded, he jumped over {maxJumpedHigh}cm after {jumps} jumps.");
+            }
+
+        }
+    }
+}
