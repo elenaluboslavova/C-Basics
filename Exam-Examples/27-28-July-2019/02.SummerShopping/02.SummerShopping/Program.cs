9e05f31188df515de1662cb46be7614cb02b71f4
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 18:09:06 +0300

    Add files via upload

---
+++ Exam-Examples/27-28-July-2019/02.SummerShopping/02.SummerShopping/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02.SummerShopping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int budget = int.Parse(Console.ReadLine());
+            double towelPrice = double.Parse(Console.ReadLine());
+            int percent = int.Parse(Console.ReadLine());
+
+            double umbrellaPrice = towelPrice * 2 / 3;
+            double flipflopsPrice = umbrellaPrice * 75 / 100;
+            double beachbagPrice = (flipflopsPrice + towelPrice) * 1 / 3;
+
+            double sum = towelPrice + flipflopsPrice + umbrellaPrice + beachbagPrice;
+            double finalSum = sum - sum * percent / 100;
+
+            if (budget >= finalSum)
+            {
+                Console.WriteLine($"Annie's sum is {finalSum:F2} lv." +
+                    $" She has {(budget-finalSum):F2} lv. left.");
+            }
+            else
+            {
+                Console.WriteLine($"Annie's sum is {finalSum:F2} lv. She " +
+                    $"needs {(finalSum-budget):F2} lv. more.");
+            }
+
+        }
+    }
+}
