4d1701ff63263b99e2aaac2ad63b42ba8eff554c
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:15:21 +0300

    Add files via upload

---
+++ Exam-Examples/06-07-July-2019/01.PoolDay/01.PoolDay/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.PoolDay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            double fee = double.Parse(Console.ReadLine());
+            double sunbedPrice = double.Parse(Console.ReadLine());
+            double umbrellaPrice = double.Parse(Console.ReadLine());
+
+            double umbrella = Math.Ceiling(people*1.0 / 2);
+            double sunbed = Math.Ceiling(people*1.0 * 75 / 100);
+
+            double sum = people * fee + umbrella * umbrellaPrice + sunbed * sunbedPrice;
+            Console.WriteLine($"{sum:F2} lv.");
+        }
+    }
+}
