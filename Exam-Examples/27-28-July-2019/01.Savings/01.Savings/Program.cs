a6fe3c7e3382060568940f259afbfce58bc5c8d3
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 18:07:22 +0300

    Add files via upload

---
+++ Exam-Examples/27-28-July-2019/01.Savings/01.Savings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01.Savings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double monthIncome = double.Parse(Console.ReadLine());
+            int months = int.Parse(Console.ReadLine());
+            double expenses = double.Parse(Console.ReadLine());
+
+            double unexpectedExpense = monthIncome * 30 / 100;
+            double sum = monthIncome - (expenses + unexpectedExpense);
+            double percent = sum / monthIncome*100;
+            Console.WriteLine($"She can save {percent:F2}%");
+            Console.WriteLine($"{sum*months:F2}");
+        }
+    }
+}
