d6f2d22a162a2f1a4e5e1a2dcf481e9229beda3d
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 16:40:53 +0300

    Add files via upload

---
+++ Exam/01.ChangeBureau/01.ChangeBureau/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _01.ChangeBureau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countBitcoin = int.Parse(Console.ReadLine());
+            double chineseUan = double.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double sum = countBitcoin * 1.0 * 1168 / 1.95 + chineseUan * 0.15 * 1.76 / 1.95;
+            double finalSum = sum - percent / 100 * sum;
+            Console.WriteLine($"{finalSum:F2}");
+        }
+    }
+}
