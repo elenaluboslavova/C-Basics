d6bae01bb4306e67db58687c9617c7eaa6a4f34c
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 18:12:32 +0300

    Add files via upload

---
+++ Exam-Examples/27-28-July-2019/06.TripExpenses/06.TripExpenses/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace _06.TripExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+
+            double price = 0;
+            double dailyMoney = 60;
+            double moneyLeftSum = 0;
+            double moneyLeft = 0;
+
+            double priceSum = 0;
+            int counter = 0;
+
+            for (int i = 0; i < days; i++)
+            {
+                string command = Console.ReadLine();
+                
+                while (command != "Day over")
+                {
+                    price = double.Parse(command);
+                    if (price > moneyLeft + dailyMoney)
+                    {
+                        continue;
+                    }
+                    counter++;
+                    priceSum += price;
+
+                    if (priceSum >= dailyMoney + moneyLeft)
+                    {
+                        Console.WriteLine($"Daily limit exceeded! You've bought {counter} products.");
+                        break;
+                    }
+
+                    command = Console.ReadLine();
+                }
+                
+                if (priceSum < dailyMoney + moneyLeft)
+                {
+                    moneyLeft = dailyMoney + moneyLeft - priceSum;
+                    moneyLeftSum += moneyLeft;
+                    Console.WriteLine($"Money left from today: {moneyLeftSum:F2}. You've bought {counter} products.");
+                }
+
+                price = 0;
+                priceSum = 0;
+                counter = 0;
+                moneyLeftSum = 0;
+            }
+        }
+    }
+}
