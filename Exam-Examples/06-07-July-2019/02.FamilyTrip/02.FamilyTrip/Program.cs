7d014aab18a01dbccb821709404f237f4cecde12
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-14 17:17:18 +0300

    Add files via upload

---
+++ Exam-Examples/06-07-July-2019/02.FamilyTrip/02.FamilyTrip/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02.FamilyTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int nights = int.Parse(Console.ReadLine());
+            double pricePerNight = double.Parse(Console.ReadLine());
+            int percent = int.Parse(Console.ReadLine());
+
+            if (nights>7)
+            {
+                pricePerNight = pricePerNight - pricePerNight * 5 / 100;
+            }
+            double sum = nights * pricePerNight * 1.0 + budget * percent / 100;
+
+            if (sum>budget)
+            {
+                Console.WriteLine($"{(sum-budget):F2} leva needed.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivanovi will be left with {(budget-sum):F2}" +
+                    $" leva after vacation.");
+            }
+        }
+    }
+}
