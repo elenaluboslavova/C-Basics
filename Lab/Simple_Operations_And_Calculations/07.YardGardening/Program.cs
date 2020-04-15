faf482df9f64dea04456b25ab782fddce3503e76
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:25:48 +0300

    Add files via upload

---
+++ Lab/Simple_Operations_And_Calculations/07.YardGardening/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _07.YardGardening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sqMeters = double.Parse(Console.ReadLine());
+            double sum = sqMeters * 7.61;
+            double discount = sum * 0.18;
+            double finalSum = sum - discount;
+            Console.WriteLine($"The final price is: {finalSum:F2} lv.");
+            Console.WriteLine($"The discount is: {discount:F2} lv.");
+        }
+    }
+}
