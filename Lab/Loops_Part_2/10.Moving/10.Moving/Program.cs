b91ff35735350432993b31bd9c1ab7fcc39218c0
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:10:12 +0300

    Add files via upload

---
+++ Lab/Loops_Part_2/10.Moving/10.Moving/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _10.Moving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int freeSpaceWidth = int.Parse(Console.ReadLine());
+            int freeSpaceLength = int.Parse(Console.ReadLine());
+            int freeSpaceHeigth = int.Parse(Console.ReadLine());
+
+            int roomSpace = freeSpaceHeigth * freeSpaceLength * freeSpaceWidth;
+            string command = Console.ReadLine();
+
+            while (command != "Done")
+            {
+                int boxesCount = int.Parse(command);
+                roomSpace -= boxesCount;
+                
+                if (roomSpace <= 0)
+                {
+                    Console.WriteLine($"No more free space! You need {Math.Abs(roomSpace)}" +
+                        $" Cubic meters more.");
+                    break;
+                }
+                command = Console.ReadLine();
+            }
+            if (roomSpace > 0)
+            {
+                Console.WriteLine($"{roomSpace} Cubic meters left.");
+            }
+        }
+    }
+}
