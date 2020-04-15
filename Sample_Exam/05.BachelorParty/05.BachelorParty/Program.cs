0f49c45ea791e1e2f62693394bd61bd7d191a894
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 20:20:51 +0300

    Add files via upload

---
+++ Sample_Exam/05.BachelorParty/05.BachelorParty/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05.BachelorParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int moneyForGuest = int.Parse(Console.ReadLine());
+
+            string command = Console.ReadLine();
+            int people = 0;
+            int sum = 0;
+            int finalSum = 0;
+            int finalPeople = 0;
+
+            while (command != "The restaurant is full")
+            {
+                people = int.Parse(command);
+                finalPeople += people;
+                if (people < 5)
+                {
+                    sum = 100 * people;
+                }
+                else
+                {
+                    sum = 70 * people;
+                }
+                finalSum += sum;
+                command = Console.ReadLine();
+            }
+            if (finalSum >= moneyForGuest)
+            {
+                Console.WriteLine($"You have {finalPeople} guests and {finalSum - moneyForGuest} leva left.");
+                
+            }
+            else
+            {
+                Console.WriteLine($"You have {finalPeople} guests and {finalSum} leva income, but no singer.");
+
+            }
+        }
+    }
+}
