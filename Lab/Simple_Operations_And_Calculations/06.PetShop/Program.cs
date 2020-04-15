ea222c18aa14c898320c72239424a64de1751f69
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:24:56 +0300

    Add files via upload

---
+++ Lab/Simple_Operations_And_Calculations/06.PetShop/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogFood = int.Parse(Console.ReadLine());
+            int otherFood = int.Parse(Console.ReadLine());
+            double Food1 = dogFood * 2.50;
+            double Food2 = otherFood * 4;
+            double sum = Food1 + Food2;
+            Console.WriteLine($"{sum:F2} lv.");
+
+        }
+    }
+}
