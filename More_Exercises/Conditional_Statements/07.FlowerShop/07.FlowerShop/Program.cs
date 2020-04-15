8e73ff682ad3e32bb51fa83bf09345d56a719efe
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:39:42 +0300

    Add files via upload

---
+++ More_Exercises/Conditional_Statements/07.FlowerShop/07.FlowerShop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07.FlowerShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int magnolias = int.Parse(Console.ReadLine());
+            int hyacinth = int.Parse(Console.ReadLine());
+            int roses = int.Parse(Console.ReadLine());
+            int cacti = int.Parse(Console.ReadLine());
+            double gift = double.Parse(Console.ReadLine());
+
+            double sum = magnolias * 3.25 + hyacinth * 4 + roses * 3.50 +
+                cacti * 8;
+            double finalSum = sum * 95 / 100;
+
+            if (finalSum>=gift)
+            {
+                Console.WriteLine($"She is left with {Math.Floor(finalSum-gift)}" +
+                    $" leva.");
+            }
+            else
+            {
+                Console.WriteLine($"She will have to borrow {Math.Ceiling(gift-finalSum)}" +
+                    $" leva.");
+            }
+        }
+    }
+}
