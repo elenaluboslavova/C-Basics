a14b3e5d89c0e354f3d355d2eb8e31eaa9813f8e
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:31:10 +0300

    Add files via upload

---
+++ Exercises/Simple_Operations_And_Calculations/04.TailoringWorkshop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _04.TailoringWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tables = double.Parse(Console.ReadLine());
+            double tableLength = double.Parse(Console.ReadLine());
+            double tableWidth = double.Parse(Console.ReadLine());
+
+            double tableclothArea = tables * ((tableLength + 2 * 0.30) * (tableWidth + 2 * 0.30));
+
+            double squareArea = tables * ((tableLength / 2) * (tableLength / 2));
+
+            double sumUSD = tableclothArea * 7 + squareArea * 9;
+            double sumBGN = sumUSD * 1.85;
+
+            Console.WriteLine($"{sumUSD:F2} USD");
+            Console.WriteLine($"{sumBGN:F2} BGN");
+        }
+    }
+}
