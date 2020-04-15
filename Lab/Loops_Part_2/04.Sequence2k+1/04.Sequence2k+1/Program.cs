9845181cd3d71e6c4f9cb73d9b8d1574c588314f
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:05:00 +0300

    Add files via upload

---
+++ Lab/Loops_Part_2/04.Sequence2k+1/04.Sequence2k+1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04.Sequence2k_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= n; i = 2 * i + 1)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            int i = 1;
+
+            while (i <= n)
+            {
+                Console.WriteLine(i);
+                i = 2 * i + 1;
+            }
+            
+        }
+    }
+}
