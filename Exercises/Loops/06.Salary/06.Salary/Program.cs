f977dd563204ca7aafcb33ea9e2acd82644fde65
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:14:24 +0300

    Add files via upload

---
+++ Exercises/Loops/06.Salary/06.Salary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _06.Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int salary = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string website = Console.ReadLine();
+
+                switch(website)
+                {
+                    case "Facebook":
+                        salary -= 150;
+                        break;
+                    case "Instagram":
+                        salary -= 100;
+                        break;
+                    case "Reddit":
+                        salary -= 50;
+                        break;
+                }
+                
+            }
+            if (salary <= 0)
+            {
+                Console.WriteLine("You have lost your salary.");
+            }
+            else
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
