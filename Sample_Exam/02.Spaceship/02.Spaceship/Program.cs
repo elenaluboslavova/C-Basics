723625324f04e04e57eb8b3700f00775ddc3d900
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 20:18:02 +0300

    Add files via upload

---
+++ Sample_Exam/02.Spaceship/02.Spaceship/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02.Spaceship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double shipWidth = double.Parse(Console.ReadLine());
+            double shipLength = double.Parse(Console.ReadLine());
+            double shipHeight = double.Parse(Console.ReadLine());
+            double peopleHeight = double.Parse(Console.ReadLine());
+
+            double room = 2 * 2 * (peopleHeight + 0.40);
+            double ship = shipHeight * shipLength * shipWidth;
+            double people = Math.Floor(ship / room);
+
+            if (people >= 3 && people <= 10)
+            {
+                Console.WriteLine($"The spacecraft holds {people} astronauts.");
+            }
+            else if (people < 3)
+            {
+                Console.WriteLine("The spacecraft is too small.");
+            }
+            else
+            {
+                Console.WriteLine("The spacecraft is too big.");
+            }
+        }
+    }
+}
