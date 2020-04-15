9a9a17a562e5255bf745e759f653a458d0e5df8d
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 19:36:11 +0300

    Add files via upload

---
+++ More_Exercises/Nested_Loops/05.ChallengeTheWedding/05.ChallengeTheWedding/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05.ChallengeTheWedding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int men = int.Parse(Console.ReadLine());
+            int women = int.Parse(Console.ReadLine());
+            int tables = int.Parse(Console.ReadLine());
+
+            int count = 0;
+
+            for (int i = 1; i <= men; i++)
+            {
+                for (int j = 1; j <= women; j++)
+                {
+                    count++;
+                    Console.Write($"({i} <-> {j}) ");
+                    if (count >= tables)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
