5e7471cd97e73b1b6bf6d859bb6533b26240d3b5
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:40:53 +0300

    Add files via upload

---
+++ Lab/Conditional_Statements/07.AreaOfFigures/07.AreaOfFigures/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _07.AreaOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string figure = Console.ReadLine();
+            if (figure=="square")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double area = side * side;
+                Console.WriteLine($"{area:F3}");
+            }
+            else if(figure=="rectangle")
+            {
+                double side1 = double.Parse(Console.ReadLine());
+                double side2 = double.Parse(Console.ReadLine());
+                double area = side1 * side2;
+                Console.WriteLine($"{area:F3}");
+            }
+            else if(figure=="circle")
+            {
+                double radius = double.Parse(Console.ReadLine());
+                double area = Math.PI * radius * radius;
+                Console.WriteLine($"{area:F3}");
+            }
+            else if(figure=="triangle")
+            {
+                double side = double.Parse(Console.ReadLine());
+                double height = double.Parse(Console.ReadLine());
+                double area = side * height / 2;
+                Console.WriteLine($"{area:F3}");
+            }
+        }
+    }
+}
