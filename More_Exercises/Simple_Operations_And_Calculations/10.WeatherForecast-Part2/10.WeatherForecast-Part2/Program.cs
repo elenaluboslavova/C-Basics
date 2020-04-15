239dbb0a947e1d2659ea903c801d5b13d5e265ca
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 20:00:15 +0300

    Add files via upload

---
+++ More_Exercises/Simple_Operations_And_Calculations/10.WeatherForecast-Part2/10.WeatherForecast-Part2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _10.WeatherForecast_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double degree = double.Parse(Console.ReadLine());
+
+            if (35<=degree && degree>=26.00)
+            {
+                Console.WriteLine("Hot");
+            }
+            else if(degree<=25.9 && degree>=20.1)
+            {
+                Console.WriteLine("Warm");
+            }
+            else if(degree<=20.00 && degree>=15.00)
+            {
+                Console.WriteLine("Mild");
+            }
+            else if(degree<=14.9 && degree>=12.00)
+            {
+                Console.WriteLine("Cool");
+            }
+            else if(degree<=11.9 && degree>=5.00)
+            {
+                Console.WriteLine("Cold");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
