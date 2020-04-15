2215e257bab4d87dccc3d5ad1868d4db99bffb66
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 19:39:54 +0300

    Add files via upload

---
+++ More_Exercises/Nested_Loops/07.SafePasswordsGenerator/07.SafePasswordsGenerator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _07.SafePasswordsGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int maxPasswords = int.Parse(Console.ReadLine());
+
+            char A = (char)35;
+            char B = (char)64;
+            int br = 1;
+
+            for (int x = 1; x <= a; x++)
+            {
+                for(int y = 1; y <= b; y++)
+                {
+                    br++;
+                    Console.Write($"{A}{B}{x}{y}{B}{A}|");
+                    A++;
+                    B++;
+                    if (A > 55)
+                    {
+                        A = (char)35;
+                    }
+                    if (B > 96)
+                    {
+                        B = (char)64;
+                    }
+                    if (br > maxPasswords)
+                    {
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
