c081f3a327fa97ac850cdf13a3505d3d6d58bc67
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:16:37 +0300

    Add files via upload

---
+++ Lab/Nested_Loops/05.Travelling/05.Travelling/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05.Travelling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string destination = Console.ReadLine();
+
+            while (destination != "End")
+            {
+                double moneyNeeded = double.Parse(Console.ReadLine());
+
+                double savedMoney = 0;
+
+                while (savedMoney < moneyNeeded)
+                {
+                    double currentSavedMoney = double.Parse(Console.ReadLine());
+                    savedMoney += currentSavedMoney;
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+
+                destination = Console.ReadLine();
+            }
+
+        }
+    }
+}
