0849fb4cc6861dd2a9f555e5a4cb0194d4676984
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 20:16:55 +0300

    Add files via upload

---
+++ Sample_Exam/01.SeaTrip/01.SeaTrip/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.SeaTrip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double moneyForFood = double.Parse(Console.ReadLine());
+            double moneyForSouvenirs = double.Parse(Console.ReadLine());
+            double moneyForHotel = double.Parse(Console.ReadLine());
+
+            double Z = moneyForHotel * 0.9 + moneyForHotel * 0.85 + moneyForHotel * 0.8;
+            double moneyForGas = 420 * 1.0 * 7 / 100 * 1.85;
+
+
+            double sum = moneyForFood * 3 + moneyForSouvenirs * 3 + Z + moneyForGas;
+
+            Console.WriteLine($"Money needed: {sum:F2}");
+        }
+    }
+}
