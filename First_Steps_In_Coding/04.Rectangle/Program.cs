71510bf90127223db7913af45c34ba5135309dab
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:08:09 +0300

    Add files via upload

---
+++ First_Steps_In_Coding/04.Rectangle/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04.Rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 7;
+            int area = a * b;
+            Console.WriteLine(area);
+        }
+    }
+}
