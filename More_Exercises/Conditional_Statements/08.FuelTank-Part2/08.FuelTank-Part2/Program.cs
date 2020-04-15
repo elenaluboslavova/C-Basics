a3d52c4d606329a7c85d638cd1da30c37278a02b
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:41:39 +0300

    Add files via upload

---
+++ More_Exercises/Conditional_Statements/08.FuelTank-Part2/08.FuelTank-Part2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace _08.FuelTank_Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fuelType = Console.ReadLine();
+            double fuel = double.Parse(Console.ReadLine());
+            string card = Console.ReadLine();
+
+            if (fuelType=="Gas")
+            {
+                double sum = fuel * 0.93;
+                if (card=="Yes")
+                {
+                    sum = sum - fuel * 0.08;
+                }
+                if (fuel>=20 && fuel<=25)
+                {
+                    sum = sum - sum * 8 / 100;
+                }
+                else if (fuel>25)
+                {
+                    sum = sum - sum * 10 / 100;
+                }
+                Console.WriteLine($"{sum:F2} lv.");
+            }
+            else if (fuelType=="Gasoline")
+            {
+                double sum = fuel * 2.22;
+                if (card == "Yes")
+                {
+                    sum = sum - fuel * 0.18;
+                }
+                if (fuel >= 20 && fuel <= 25)
+                {
+                    sum = sum - sum * 8 / 100;
+                }
+                else if (fuel > 25)
+                {
+                    sum = sum - sum * 10 / 100;
+                }
+                Console.WriteLine($"{sum:F2} lv.");
+            }
+            else if (fuelType=="Diesel")
+            {
+                double sum = fuel * 2.33;
+                if (card == "Yes")
+                {
+                    sum = sum - fuel * 0.12;
+                }
+                if (fuel >= 20 && fuel <= 25)
+                {
+                    sum = sum - sum * 8 / 100;
+                }
+                else if (fuel > 25)
+                {
+                    sum = sum - sum * 10 / 100;
+                }
+                Console.WriteLine($"{sum:F2} lv.");
+            }
+        }
+    }
+}
