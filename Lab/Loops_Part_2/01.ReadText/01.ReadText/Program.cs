5d575558c6d0e8d0ce47f918992bcfd49c2a5502
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:02:08 +0300

    Add files via upload

---
+++ Lab/Loops_Part_2/01.ReadText/01.ReadText/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01.ReadText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "Stop")
+            {
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
