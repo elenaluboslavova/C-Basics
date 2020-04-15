1c12eee631e83dfa7bfd1ac765074d735d467b86
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:19:43 +0300

    Add files via upload

---
+++ Exercises/Loops_Part_2/05.Coins/05.Coins/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _05.Coins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double change = double.Parse(Console.ReadLine());
+
+            double changeInSt = Math.Floor(change * 100);
+            int counter = 0;
+
+            while (changeInSt > 0)
+            {
+                if (changeInSt >= 200)
+                {
+                    changeInSt -= 200;
+                    counter++;
+                }
+                else if (changeInSt >= 100)
+                {
+                    changeInSt -= 100;
+                    counter++;
+                }
+                else if (changeInSt >= 50)
+                {
+                    changeInSt -= 50;
+                    counter++;
+                }
+                else if (changeInSt >= 20)
+                {
+                    changeInSt -= 20;
+                    counter++;
+                }
+                else if (changeInSt >= 10)
+                {
+                    changeInSt -= 10;
+                    counter++;
+                }
+                else if (changeInSt >= 5)
+                {
+                    changeInSt -= 5;
+                    counter++;
+                }
+                else if (changeInSt >= 2)
+                {
+                    changeInSt -= 2;
+                    counter++;
+                }
+                else if (changeInSt >= 1)
+                {
+                    changeInSt -= 1;
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
