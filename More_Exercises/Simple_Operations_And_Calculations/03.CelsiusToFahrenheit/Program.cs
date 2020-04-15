7df5b2e1fd8fdf6231a38b36fe37038679fd0e90
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 19:51:43 +0300

    Add files via upload

---
+++ More_Exercises/Simple_Operations_And_Calculations/03.CelsiusToFahrenheit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _03.CelsiusToFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius = double.Parse(Console.ReadLine());
+            double fahrenheit = celsius * 1.80000 + 32.00;
+            Console.WriteLine($"{fahrenheit:F2}");
+        }
+    }
+}
