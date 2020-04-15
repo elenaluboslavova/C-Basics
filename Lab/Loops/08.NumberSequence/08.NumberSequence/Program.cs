1e1cece802c37d4f53fedfab758a750d70314e8a
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:58:46 +0300

    Add files via upload

---
+++ Lab/Loops/08.NumberSequence/08.NumberSequence/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _08.NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int smallest = int.MaxValue;
+            int biggest = int.MinValue;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                
+                if (currentNum<smallest)
+                {
+                    smallest = currentNum;
+                }
+
+                if (currentNum>biggest)
+                {
+                    biggest = currentNum;
+                }
+            }
+            Console.WriteLine($"Max number: {biggest}");
+            Console.WriteLine($"Min number: {smallest}");
+        }
+    }
+}
