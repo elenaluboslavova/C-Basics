4f9bc0beff69cea01042a66aed38f2872c5470bf
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 19:55:55 +0300

    Add files via upload

---
+++ More_Exercises/Simple_Operations_And_Calculations/06.Fishland/06.Fishland/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06.Fishland
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //скумрия
+            double mackerelPrice = double.Parse(Console.ReadLine());
+            //цаца
+            double spratPrice = double.Parse(Console.ReadLine());
+            //паламуд
+            double chamoisKg = double.Parse(Console.ReadLine());
+            //сафрид
+            double scadKg = double.Parse(Console.ReadLine());
+            //миди
+            double musselsKg = double.Parse(Console.ReadLine());
+
+            double chamoisPrice = mackerelPrice + mackerelPrice * 6 / 10;
+            double scadPrice = spratPrice + spratPrice * 8 / 10;
+            double musselsPrice = 7.50;
+
+            double sum = chamoisKg * chamoisPrice + scadKg * scadPrice + musselsKg * musselsPrice;
+            Console.WriteLine($"{sum:F2}");
+        }
+    }
+}
