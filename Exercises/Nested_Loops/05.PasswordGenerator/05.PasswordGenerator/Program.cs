928eec1608bf67c2c4fe613f653410a98e35eed1
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:25:50 +0300

    Add files via upload

---
+++ Exercises/Nested_Loops/05.PasswordGenerator/05.PasswordGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _05.PasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int l = int.Parse(Console.ReadLine());
+
+            int asciiCode = 96;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    for (char k = 'a'; k <= asciiCode + l; k++)
+                    {
+                        for (char m = 'a'; m <= asciiCode + l; m++)
+                        {
+                            for (int x = 1; x <= n; x++)
+                            {
+                                if (x > i && x > j)
+                                {
+                                    Console.Write($"{i}{j}{k}{m}{x} ");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
