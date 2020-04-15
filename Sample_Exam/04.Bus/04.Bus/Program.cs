9b0bc31408e6587cf0eb9e252efab2c04fddb9ba
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 20:19:56 +0300

    Add files via upload

---
+++ Sample_Exam/04.Bus/04.Bus/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _04.Bus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int passengersStart = int.Parse(Console.ReadLine());
+            int stops = int.Parse(Console.ReadLine());
+
+            int leaving = 0;
+            int coming = 0;
+            for (int i = 1; i <= stops; i++)
+            {
+                leaving = int.Parse(Console.ReadLine());
+                coming = int.Parse(Console.ReadLine());
+                if (i % 2 == 1)
+                {
+                    coming += 2;
+                }
+                else
+                {
+                    leaving += 2;
+                }
+                passengersStart -= leaving;
+                passengersStart += coming;
+                
+            }
+            Console.WriteLine($"The final number of passengers is : {passengersStart}");
+        }
+    }
+}
