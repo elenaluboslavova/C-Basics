449053915c65d5ff0dc2f16829d36a6cba1889db
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:30:57 +0300

    Add files via upload

---
+++ More_Exercises/Conditional_Statements/02.SleepyTomCat/02.SleepyTomCat/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _02.SleepyTomCat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int freeDays = int.Parse(Console.ReadLine());
+            int playingMinutes = freeDays * 127 + (365 - freeDays) * 63;
+            if (playingMinutes>30000)
+            {
+                Console.WriteLine("Tom will run away");
+                Console.WriteLine($"{(playingMinutes-30000)/60} hours and " +
+                    $"{(playingMinutes-30000)%60} minutes more for play");
+            }
+            else
+            {
+                Console.WriteLine("Tom sleeps well");
+                Console.WriteLine($"{(30000-playingMinutes)/60} hours" +
+                    $" and {(30000-playingMinutes)%60} minutes less for play");
+            }
+        }
+    }
+}
