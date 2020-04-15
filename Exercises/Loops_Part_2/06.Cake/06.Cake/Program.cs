9c9c3501ec6cb96075b63112049625e932e1fc3b
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:20:32 +0300

    Add files via upload

---
+++ Exercises/Loops_Part_2/06.Cake/06.Cake/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06.Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+
+            int cake = width * length;
+            string command = "";
+            int pieceOfCake = 0;
+            int piecesCount = 0;
+
+            while (piecesCount < cake)
+            {
+                command = Console.ReadLine();
+                if (command=="STOP")
+                {
+                    Console.WriteLine($"{cake-piecesCount} pieces are left.");
+                    break;
+                }
+                pieceOfCake = int.Parse(command);
+                piecesCount += pieceOfCake;
+                
+            }
+            if (piecesCount >= cake)
+            {
+                Console.WriteLine($"No more cake left! You need {piecesCount - cake} pieces more.");
+            }
+
+        }
+    }
+}
