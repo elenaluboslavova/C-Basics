343d06aa589cc62fe2bff14f85c1c014278c0a0a
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 20:21:53 +0300

    Add files via upload

---
+++ Sample_Exam/06.MultiplyTable/06.MultiplyTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _06.MultiplyTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            int one = Convert.ToInt32(number[0])-48;
+            int two = Convert.ToInt32(number[1])-48;
+            int three = Convert.ToInt32(number[2])-48;
+
+
+            for (int i = 1; i <= three; i++)
+            {
+                for (int j = 1; j <= two; j++)
+                {
+                    for (int l = 1; l <= one; l++)
+                    {
+                        Console.WriteLine($"{i} * {j} * {l} = {i*j*l};");
+                    }
+                }
+            }
+        }
+    }
+}
