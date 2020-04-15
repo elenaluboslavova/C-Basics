a029570f4f9926a3b02002b162c33c72ef72a76f
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:01:39 +0300

    Add files via upload

---
+++ Exercises/Conditional_Statements_Advanced/07.OperationsBetweenNumbers/07.OperationsBetweenNumbers/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace _07.OperationsBetweenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N1 = int.Parse(Console.ReadLine());
+            int N2 = int.Parse(Console.ReadLine());
+            char symbol = char.Parse(Console.ReadLine());
+
+            string a = "";
+            
+            switch (symbol)
+            {
+                case '+':
+                    if ((N1+N2)%2==0)
+                    {
+                        a = "even";
+                    }
+                    else
+                    {
+                        a = "odd";
+                    }
+                    Console.WriteLine($"{N1} + {N2} = {N1+N2} - {a}");
+                    break;
+
+                case '-':
+                    if ((N1 - N2) % 2 == 0)
+                    {
+                        a = "even";
+                    }
+                    else
+                    {
+                        a = "odd";
+                    }
+                    Console.WriteLine($"{N1} - {N2} = {N1 - N2} - {a}");
+                    break;
+
+                case '*':
+                    if ((N1 * N2) % 2 == 0)
+                    {
+                        a = "even";
+                    }
+                    else
+                    {
+                        a = "odd";
+                    }
+                    Console.WriteLine($"{N1} * {N2} = {N1 * N2} - {a}");
+                    break;
+
+                case '/':
+                    if (N2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {N1} by zero");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{N1} / {N2} = {(N1 * 1.0 / N2):F2}");
+                    }
+                    break;
+
+                case '%':
+                    if (N2 == 0)
+                    {
+                        Console.WriteLine($"Cannot divide {N1} by zero");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{N1} % {N2} = {N1 % N2}");
+                    }
+                    break;
+            }
+
+            
+        }
+    }
+}
