0a1acfa54b802f7d9b910913b19489ee3b369a9a
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:32:53 +0300

    Add files via upload

---
+++ Exercises/Simple_Operations_And_Calculations/06.CharityCampaign/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06.CharityCampaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int bakers = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int waffles = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+
+            double cakePrice = 45;
+            double wafflePrice = 5.80;
+            double pancakePrice = 3.20;
+
+            double sum = days * bakers * 1.0 * (cakes * cakePrice +
+                waffles * wafflePrice + pancakes * pancakePrice);
+            double finalSum = sum - sum * 1 / 8;
+            Console.WriteLine($"{finalSum:F2}");
+        }
+    }
+}
