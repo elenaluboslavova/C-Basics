19b2c49573d3f90e42ba9cc8c5fc146781733549
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:23:13 +0300

    Add files via upload

---
+++ Lab/Simple_Operations_And_Calculations/04.InchesToCentimeters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SantimetersToInches
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double sant = double.Parse(Console.ReadLine());
+            double inch = sant * 2.54;
+            Console.WriteLine(inch);
+        }
+    }
+}
