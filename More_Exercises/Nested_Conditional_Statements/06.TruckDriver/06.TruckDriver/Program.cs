17260ac9fc49838a7fd86d0d90cb1d1bdd2b21bb
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 19:21:37 +0300

    Add files via upload

---
+++ More_Exercises/Nested_Conditional_Statements/06.TruckDriver/06.TruckDriver/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace _06.TruckDriver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string season = Console.ReadLine();
+            double kmPerMonth = double.Parse(Console.ReadLine());
+
+            double sum = 0;
+
+            if (season == "Spring" || season == "Autumn")
+            {
+                if (kmPerMonth <= 5000)
+                {
+                    sum = kmPerMonth * 0.75;
+                }
+                else if (kmPerMonth > 5000 && kmPerMonth <= 10000)
+                {
+                    sum = kmPerMonth * 0.95;
+                }
+                else if (kmPerMonth > 10000 && kmPerMonth <= 20000)
+                {
+                    sum = kmPerMonth * 1.45;
+                }
+            }
+
+
+            else if (season == "Summer")
+            {
+                if (kmPerMonth <= 5000)
+                {
+                    sum = kmPerMonth * 0.90;
+                }
+                else if (kmPerMonth > 5000 && kmPerMonth <= 10000)
+                {
+                    sum = kmPerMonth * 1.10;
+                }
+                else if (kmPerMonth > 10000 && kmPerMonth <= 20000)
+                {
+                    sum = kmPerMonth * 1.45;
+                }
+            }
+
+
+            else if (season == "Winter")
+            {
+                if (kmPerMonth <= 5000)
+                {
+                    sum = kmPerMonth * 1.05;
+                }
+                else if (kmPerMonth > 5000 && kmPerMonth <= 10000)
+                {
+                    sum = kmPerMonth * 1.25;
+                }
+                else if (kmPerMonth > 10000 && kmPerMonth <= 20000)
+                {
+                    sum = kmPerMonth * 1.45;
+                }
+            }
+
+            sum = sum - sum * 0.1;
+            Console.WriteLine($"{sum*4:F2}");
+        }
+    }
+}
