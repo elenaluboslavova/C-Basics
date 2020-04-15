4475cbe3fa7652493c19331d3a550e76bff680b7
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:38:47 +0300

    Add files via upload

---
+++ More_Exercises/Conditional_Statements/06.Pets/06.Pets/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06.Pets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int food = int.Parse(Console.ReadLine());
+            double dogFood = double.Parse(Console.ReadLine());
+            double catFood = double.Parse(Console.ReadLine());
+            double turtleFood = double.Parse(Console.ReadLine());
+
+            double neededFood = (dogFood + catFood + turtleFood / 1000) * days;
+            if (neededFood>food)
+            {
+                Console.WriteLine($"{Math.Ceiling(neededFood-food)} more kilos of food are needed.");
+            }
+            else
+            {
+                Console.WriteLine($"{Math.Floor(food-neededFood)} kilos of food left.");
+            }
+        }
+    }
+}
