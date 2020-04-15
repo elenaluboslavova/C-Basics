e424aa0e34d4cd1e076443fb538dc5555b87b919
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:07:36 +0300

    Add files via upload

---
+++ Lab/Loops_Part_2/07.MinNumber/07.MinNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07.MinNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int minNum = int.MaxValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number<minNum)
+                {
+                    minNum = number;
+                }
+            }
+            Console.WriteLine(minNum);
+        }
+    }
+}
