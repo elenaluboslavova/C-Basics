756b4405fdf944f2ce348688803121f04144191a
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 15:57:02 +0300

    Add files via upload

---
+++ Exercises/Conditional_Statements_Advanced/02.Cinema/02.Cinema/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02.Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string typeMovie = Console.ReadLine();
+            int r = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            switch(typeMovie)
+            {
+                case "Premiere": Console.WriteLine($"{(r * c * 12.00):F2}");
+                    break;
+                case "Normal": Console.WriteLine($"{(r * c * 7.50):F2}");
+                    break;
+                case "Discount": Console.WriteLine($"{(r * c * 5.00):F2}");
+                    break;
+            }
+        }
+    }
+}
