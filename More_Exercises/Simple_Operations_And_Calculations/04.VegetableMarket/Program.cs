2efa94e976b7f110f4453c6dc507bcd8d40c3418
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 19:53:12 +0300

    Add files via upload

---
+++ More_Exercises/Simple_Operations_And_Calculations/04.VegetableMarket/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _04.VegetableMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double N = double.Parse(Console.ReadLine());
+            double M = double.Parse(Console.ReadLine());
+            double vegKg = double.Parse(Console.ReadLine());
+            double fruitKg = double.Parse(Console.ReadLine());
+
+            double veg = N * vegKg;
+            double fruit = M * fruitKg;
+            double sum = veg + fruit;
+            Console.WriteLine($"{sum/1.94:F2}");
+        }
+    }
+}
