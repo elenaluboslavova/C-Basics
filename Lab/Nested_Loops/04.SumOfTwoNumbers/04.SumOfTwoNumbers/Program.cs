a612b5f0e9f21c21a89f8287253abaa083eea2cf
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:15:02 +0300

    Add files via upload

---
+++ Lab/Nested_Loops/04.SumOfTwoNumbers/04.SumOfTwoNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04.SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            int counter = 0;
+
+            for (int i = x; i <= y; i++)
+            {
+                for (int k = x; k <= y; k++)
+                {
+                    counter++;
+                    sum = i + k;
+                    if (sum == magicNumber)
+                    {
+                        Console.WriteLine($"Combination N:{counter}" +
+                            $" ({i} + {k} = {magicNumber})");
+                        return;
+                    }
+                }
+                
+            }
+            if (sum != magicNumber)
+            {
+                Console.WriteLine($"{counter} combinations - " +
+                            $"neither equals {magicNumber}");
+            }
+        }
+    }
+}
