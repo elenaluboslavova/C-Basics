5010715d5d57a2a8bcbd437c8404e8c60d1617f4
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 19:27:06 +0300

    Add files via upload

---
+++ More_Exercises/Nested_Conditional_Statements/09.MultiplyBy2/09.MultiplyBy2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _09.MultiplyBy2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = 0;
+            for (int i = 0; i < 100; i++)
+            {
+                number = double.Parse(Console.ReadLine());
+                if (number < 0)
+                {
+                    Console.WriteLine("Negative number!");
+                    Environment.Exit(0);
+                }
+                Console.WriteLine($"Result: {number * 2:F2}");
+            }
+        }
+    }
+}
