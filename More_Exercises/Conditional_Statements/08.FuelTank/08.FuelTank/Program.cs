89c1319c4e0d6cc0fb165951aa766b17a78f48bc
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:40:34 +0300

    Add files via upload

---
+++ More_Exercises/Conditional_Statements/08.FuelTank/08.FuelTank/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _08.FuelTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fuelType = Console.ReadLine();
+            int fuel = int.Parse(Console.ReadLine());
+
+            if (fuelType=="Diesel")
+            {
+                if (fuel>=25)
+                {
+                    Console.WriteLine($"You have enough diesel.");
+                }
+                else
+                {
+                    Console.WriteLine($"Fill your tank with diesel!");
+                }
+            }
+            else if(fuelType=="Gasoline")
+            {
+                if (fuel >= 25)
+                {
+                    Console.WriteLine($"You have enough gasoline.");
+                }
+                else
+                {
+                    Console.WriteLine($"Fill your tank with gasoline!");
+                }
+            }
+            else if(fuelType=="Gas")
+            {
+                if (fuel >= 25)
+                {
+                    Console.WriteLine($"You have enough gas.");
+                }
+                else
+                {
+                    Console.WriteLine($"Fill your tank with gas!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid fuel!");
+            }
+        }
+    }
+}
