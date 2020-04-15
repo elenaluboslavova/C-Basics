8977b439d725823d87171662ed537b80a1308839
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:12:29 +0300

    Add files via upload

---
+++ Exercises/Loops/04.Histogram/04.Histogram/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _04.Histogram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int p1 = 0;
+            int p2 = 0;
+            int p3 = 0;
+            int p4 = 0;
+            int p5 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num<200)
+                {
+                    p1++;
+                }
+                else if(num>=200 && num<=399)
+                {
+                    p2++;
+                }
+                else if(num>=400 && num<=599)
+                {
+                    p3++;
+                }
+                else if(num>=600 && num<=799)
+                {
+                    p4++;
+                }
+                else if(num>=800)
+                {
+                    p5++;
+                }
+            }
+
+            Console.WriteLine($"{(p1 * 1.0 / n * 100):F2}%");
+            Console.WriteLine($"{(p2 * 1.0 / n * 100):F2}%");
+            Console.WriteLine($"{(p3 * 1.0 / n * 100):F2}%");
+            Console.WriteLine($"{(p4 * 1.0 / n * 100):F2}%");
+            Console.WriteLine($"{(p5 * 1.0 / n * 100):F2}%");
+        }
+    }
+}
