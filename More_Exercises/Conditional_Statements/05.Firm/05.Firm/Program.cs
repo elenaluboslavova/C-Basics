db0e32f93c742c17ac9c163142f44b91f9b5fb82
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:33:34 +0300

    Add files via upload

---
+++ More_Exercises/Conditional_Statements/05.Firm/05.Firm/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _05.Firm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int days = int.Parse(Console.ReadLine());
+            int workers = int.Parse(Console.ReadLine());
+
+            double workingHours = (days * 1.0 - 10 / 100 * days) * 8;
+            double overtime = workers * days * 2 * 1.0;
+            double finalHours = Math.Floor(workingHours + overtime);
+            
+            if (finalHours<hours)
+            {
+                Console.WriteLine($"Not enough time! {hours-finalHours} hours needed.");
+            }
+            else
+            {
+                Console.WriteLine($"Yes! {finalHours-hours} hours left.");
+            }
+        }
+    }
+}
