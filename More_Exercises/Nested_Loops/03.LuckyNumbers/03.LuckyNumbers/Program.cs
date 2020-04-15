f4f0968cd97ee826c5dbb9ca4653f8d862583064
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 19:33:02 +0300

    Add files via upload

---
+++ More_Exercises/Nested_Loops/03.LuckyNumbers/03.LuckyNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _03.LuckyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 9; i++)
+            {
+                for (int j = 1; j <= 9; j++)
+                {
+                    for (int l = 1; l <= 9; l++)
+                    {
+                        for (int m = 1; m <= 9; m++)
+                        {
+                            if (i + j == l + m && n % (i + j) == 0)
+                            {
+                                Console.Write($"{i}{j}{l}{m} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
