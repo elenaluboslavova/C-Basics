199d3fbd690b17909684ba78ddac21c1629f4527
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:28:24 +0300

    Add files via upload

---
+++ Exercises/Simple_Operations_And_Calculations/01.USDtoBGN/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01.USDtoBGN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double usd = double.Parse(Console.ReadLine());
+            double bgn = usd * 1.79546;
+            Console.WriteLine($"{bgn:F2} BGN");
+        }
+    }
+}
