1e625c45b477860fb4d3908f41e8cd65634e78f3
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:15:45 +0300

    Add files via upload

---
+++ Exercises/Loops_Part_2/01.OldBooks/01.OldBooks/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01.OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string favouriteBook = Console.ReadLine();
+            int allBooks = int.Parse(Console.ReadLine());
+            string book = "";
+            int counter = 0;
+
+            while (book != favouriteBook)
+            {
+                book = Console.ReadLine();
+                
+                allBooks--;
+                if (book==favouriteBook)
+                {
+                    Console.WriteLine($"You checked {counter} books and found it.");
+                    Environment.Exit(0);
+                }
+                counter++;
+                if (allBooks==0)
+                {
+                    Console.WriteLine("The book you search is not here!");
+                    Console.WriteLine($"You checked {counter} books.");
+                    Environment.Exit(0);
+                }
+                
+            }
+            
+        }
+    }
+}
