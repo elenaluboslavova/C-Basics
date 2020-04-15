ecbec4ad05ecb76817078af84f94b3ee6a7c0610
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 18:58:05 +0300

    Add files via upload

---
+++ More_Exercises/For-Loop/02.Hospital/02.Hospital/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _02.Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int patients = 0;
+            int nPatients = 0;
+            int pPatients = 0;
+            int nPatientsCount = 0;
+            int pPatientsCount = 0;
+
+            for (int i = 1; i <= days; i++)
+            {
+                patients = int.Parse(Console.ReadLine());
+                if (i % 3 == 0 && nPatientsCount > pPatientsCount)
+                {
+                    if (patients >= 8)
+                    {
+                        pPatients = 8;
+                        nPatients = patients - 8;
+                    }
+                    else
+                    {
+                        pPatients = patients;
+                        nPatients = 0;
+                    }
+                }
+                else
+                {
+                    if (patients >= 7)
+                    {
+                        pPatients = 7;
+                        nPatients = patients - 7;
+                    }
+                    else
+                    {
+                        pPatients = patients;
+                        nPatients = 0;
+                    }
+                }
+                
+                nPatientsCount += nPatients;
+                pPatientsCount += pPatients;
+            }
+            Console.WriteLine($"Treated patients: {pPatientsCount}.");
+            Console.WriteLine($"Untreated patients: {nPatientsCount}.");
+        }
+    }
+}
