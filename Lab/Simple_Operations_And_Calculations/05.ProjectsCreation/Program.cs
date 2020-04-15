7cef856ea5ebae5f137a251511157484300929ac
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:24:03 +0300

    Add files via upload

---
+++ Lab/Simple_Operations_And_Calculations/05.ProjectsCreation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string architect = Console.ReadLine();
+            int projects = int.Parse(Console.ReadLine());
+            Console.WriteLine($"The architect {architect} will need { projects*3} hours to complete { projects} project/s.");
+        }
+    }
+}
