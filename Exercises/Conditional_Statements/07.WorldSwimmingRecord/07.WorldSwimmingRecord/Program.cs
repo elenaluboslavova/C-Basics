02f5570fd36512a73c5ff0a03e24d7ad3135d416
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 15:52:52 +0300

    Add files via upload

---
+++ Exercises/Conditional_Statements/07.WorldSwimmingRecord/07.WorldSwimmingRecord/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07.WorldSwimmingRecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double recordTime = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double secondsPerMeter = double.Parse(Console.ReadLine());
+
+
+            double firstTime = distance * secondsPerMeter;
+            double resistance = Math.Floor(distance / 15) * 12.5;
+            double time = firstTime + resistance;
+            if (recordTime>time)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world" +
+                    $" record is {time:F2} seconds.");
+
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was " +
+                    $"{(time-recordTime):F2} seconds slower.");
+            }
+        }
+    }
+}
