2b616abb1816a25eacec866a55e3422735d65432
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:24:33 +0300

    Add files via upload

---
+++ Exercises/Nested_Loops/04.TrainTheTrainers/04.TrainTheTrainers/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _04.TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int juryCount = int.Parse(Console.ReadLine());
+
+            string presentation = Console.ReadLine();
+            double grade = 0;
+            double finalGrade = 0;
+            double final = 0;
+            int counter = 0;
+
+            while (presentation != "Finish")
+            {
+                finalGrade = 0;
+                counter++;
+                for (int i = 0; i < juryCount; i++)
+                {
+                    grade = double.Parse(Console.ReadLine());
+                    finalGrade += grade;
+                }
+                final += finalGrade;
+                Console.WriteLine($"{presentation} - {finalGrade/juryCount:F2}.");
+                presentation = Console.ReadLine();
+            }
+            if (presentation == "Finish")
+            {
+                Console.WriteLine($"Student's final assessment is {final / counter / juryCount:F2}.");
+                return;
+            }
+            
+        }
+    }
+}
