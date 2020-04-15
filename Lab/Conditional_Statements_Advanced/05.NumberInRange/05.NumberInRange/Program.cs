8c4a890e2541dbc446e22638662096c262546593
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:45:44 +0300

    Add files via upload

---
+++ Lab/Conditional_Statements_Advanced/05.NumberInRange/05.NumberInRange/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _05.NumberInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number>=-100 && number!=0 && number<=100)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
