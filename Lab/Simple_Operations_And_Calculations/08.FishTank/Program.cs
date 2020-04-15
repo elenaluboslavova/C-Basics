cb072d197c36186313549ffaeec327e6a2be719d
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:26:50 +0300

    Add files via upload

---
+++ Lab/Simple_Operations_And_Calculations/08.FishTank/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08.FishTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int heigth = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+            int volume = length * width * heigth;
+            double volumeInLiters = volume * 0.001;
+            double numberPercent = percent * 0.01;
+            double freeSpace = volumeInLiters * (1 - numberPercent);
+            Console.WriteLine($"{ freeSpace:F3}");
+        }
+    }
+}
