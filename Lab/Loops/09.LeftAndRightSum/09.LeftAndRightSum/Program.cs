402548693b5a084e5677d2559aee0772a4e3a1fe
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:59:34 +0300

    Add files via upload

---
+++ Lab/Loops/09.LeftAndRightSum/09.LeftAndRightSum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09.LeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum1 = 0;
+            int sum2 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum1 += currentNum;
+                
+            }
+            for (int i = 0; i < n ; i++)
+            {
+                int currentNum1 = int.Parse(Console.ReadLine());
+                sum2 += currentNum1;
+            }
+
+            if (sum1==sum2)
+            {
+                Console.WriteLine($"Yes, sum = {sum1}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(sum1-sum2)}");
+            }
+        }
+    }
+}
