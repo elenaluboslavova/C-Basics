76a85ae36f93f2cec51a0e1282a23227915004c8
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:06:42 +0300

    Add files via upload

---
+++ Lab/Loops_Part_2/06.MaxNumber/06.MaxNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _06.MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int max = int.MinValue;
+            int i = 0;
+
+            while (i<n)
+            {
+                int num = int.Parse(Console.ReadLine());
+                i++;
+                
+                if (num > max)
+                {
+                    max = num;
+                }
+                
+            }
+            Console.WriteLine(max);
+        }
+    }
+}
