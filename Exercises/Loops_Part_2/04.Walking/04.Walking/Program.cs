b326061d44172cf94cd8d278423fff111d49f52b
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:18:28 +0300

    Add files via upload

---
+++ Exercises/Loops_Part_2/04.Walking/04.Walking/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _04.Walking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int steps = 0;
+            int stepsSum = 0;
+
+            while (stepsSum<10000)
+            {
+                string command = Console.ReadLine();
+                
+
+                if (command=="Going home")
+                {
+                    int stepsToHome = int.Parse(Console.ReadLine());
+                    stepsSum += stepsToHome;
+                    break;
+
+                }
+                else
+                {
+                    steps = int.Parse(command);
+                    stepsSum += steps;
+                    if (stepsSum >= 10000)
+                    {
+                        break;
+                    }
+                }
+            }
+            if (stepsSum>=10000)
+            {
+                Console.WriteLine("Goal reached! Good job!");
+            }
+            else
+            {
+                Console.WriteLine($"{10000-stepsSum} more steps to reach goal.");
+            }
+        }
+    }
+}
