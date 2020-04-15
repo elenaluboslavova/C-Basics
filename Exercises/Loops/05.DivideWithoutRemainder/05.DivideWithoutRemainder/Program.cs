9ceba6e58f0e76bfda71d9911b47e506e01226a4
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:13:20 +0300

    Add files via upload

---
+++ Exercises/Loops/05.DivideWithoutRemainder/05.DivideWithoutRemainder/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _05.DivideWithoutRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersCount = int.Parse(Console.ReadLine());
+            int p1 = 0;
+            int p2 = 0;
+            int p3 = 0;
+
+            for(int i = 0; i < numbersCount; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+
+                if (currentNum % 2 == 0)
+                {
+                    p1++;
+                }
+                if (currentNum % 3 == 0)
+                {
+                    p2++;
+                }
+                if (currentNum % 4 == 0)
+                {
+                    p3++;
+                }
+            }
+            double P1 = p1 * 1.0 / numbersCount * 100;
+            double P2 = p2 * 1.0 / numbersCount * 100;
+            double P3 = p3 * 1.0 / numbersCount * 100;
+
+            Console.WriteLine($"{P1:F2}%");
+            Console.WriteLine($"{P2:F2}%");
+            Console.WriteLine($"{P3:F2}%");
+        }
+    }
+}
