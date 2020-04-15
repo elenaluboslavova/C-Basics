6ed4e828945fbc13b324a5fffcd2dc99f26b6e6c
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 17:05:53 +0300

    Add files via upload

---
+++ Lab/Loops_Part_2/05.AccountBalance/05.AccountBalance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _05.AccountBalance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i = 0;
+            double sum = 0;
+
+            while (i < n)
+            {
+                double payment = double.Parse(Console.ReadLine());
+                
+
+                if (payment < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                Console.WriteLine($"Increase: {payment:F2}");
+                i++;
+                sum += payment;
+            }
+
+            Console.WriteLine($"Total: {sum:F2}");
+        }
+    }
+}
