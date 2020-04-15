58191a7844c688e75910901a6fb8bf511422e563
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:32:04 +0300

    Add files via upload

---
+++ Exercises/Simple_Operations_And_Calculations/05.DanceHall/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _05.DanceHall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double L = double.Parse(Console.ReadLine());
+            double W = double.Parse(Console.ReadLine());
+            double A = double.Parse(Console.ReadLine());
+            double hallArea = L * 100 * W * 100;
+            double bench =  hallArea / 10;
+            double wardrobeArea = A * 100 * A * 100;
+            double dancerArea = 40 + 7000;
+            double dancers = (hallArea - wardrobeArea - bench) / dancerArea;
+            Console.WriteLine(Math.Floor(dancers));
+        }
+    }
+}
