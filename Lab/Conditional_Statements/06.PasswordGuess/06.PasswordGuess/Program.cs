ee48d8beee93af67a8f1bf6a79a33535427e8ab0
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:40:13 +0300

    Add files via upload

---
+++ Lab/Conditional_Statements/06.PasswordGuess/06.PasswordGuess/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06.PasswordGuess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            string rightPassword = "s3cr3t!P@ssw0rd";
+            if (password==rightPassword)
+            {
+                Console.WriteLine("Welcome");
+            }
+            else
+            {
+                Console.WriteLine("Wrong password!");
+            }
+        }
+    }
+}
