d758813ae70b07c07a371b66ba259dd6839f1d17
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:10:47 +0300

    Add files via upload

---
+++ Exercises/Loops/02.HalfSumElement/02.HalfSumElement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _02.HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int max = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+
+                if (currentNum>max)
+                {
+                    max = currentNum;
+                }
+            }
+            int sumWithoutMaxNumber = sum - max;
+            if (max == sumWithoutMaxNumber)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {max}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(max-sumWithoutMaxNumber)}");
+            }
+        }
+    }
+}
