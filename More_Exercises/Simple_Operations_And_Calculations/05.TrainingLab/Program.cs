be5050cf7a7d3a4ebbfd4ffa5863106b5b1bddc7
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 19:54:27 +0300

    Add files via upload

---
+++ More_Exercises/Simple_Operations_And_Calculations/05.TrainingLab/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _05.TrainingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+
+            double row = Math.Floor((h * 100 - 100) / 70);
+            double column = Math.Floor(w * 100 / 120);
+            double seats = Math.Floor(row * column - 3);
+            Console.WriteLine(seats);
+        }
+    }
+}
