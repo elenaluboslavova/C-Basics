237cf8f3735afc147dd19f0ef37c58e71c54f610
Elena Boyadzhieva <63240718+elenaluboslavova@users.noreply.github.com>
2020-04-15 16:57:56 +0300

    Add files via upload

---
+++ Lab/Loops/07.SumNumbers/07.SumNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _07.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int currentNum = int.Parse(Console.ReadLine());
+                sum += currentNum;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
